d3cd6f529bdff50fa293f5d04aef65c70bf066c1
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-03-01 22:25:35 +1000

    Added DZ13

---
+++ DZ13/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if(a<100&&a>-99) System.Console.WriteLine("Третьей цифры нет");
+else 
+{
+    string value = Convert.ToString(a);
+    System.Console.WriteLine(value[2]);
+}
