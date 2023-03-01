e91bbb826344934a8f00b59fc20ba4abbf6c000b
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-03-01 22:30:19 +1000

    Added DZ15

---
+++ DZ15/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+switch(a)
+{
+    case 1: System.Console.WriteLine("No");
+    break;
+    case 2:System.Console.WriteLine("No");
+    break;
+    case 3:System.Console.WriteLine("No");
+    break;
+    case 4:System.Console.WriteLine("No");
+    break;
+    case 5:System.Console.WriteLine("No");
+    break;
+    case 6:System.Console.WriteLine("Yes");
+    break;
+    case 7:System.Console.WriteLine("Yes");
+    break;
+}
