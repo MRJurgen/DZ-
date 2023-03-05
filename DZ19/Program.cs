82a97c23063ffc296f9fdc01f06c5baee31fbb4b
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-03-05 22:14:57 +1000

    Added num 19

---
+++ DZ19/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Ввидите число: ");
+string a = Console.ReadLine();
+if(a.Length ==5)
+{
+    if(a[0]==a[4]&&a[1]==a[3])
+    {
+        System.Console.WriteLine("Да");
+    }
+    else System.Console.WriteLine("Нет");
+}
+else System.Console.WriteLine("Ошибка в формате числа");
