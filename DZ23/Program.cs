326a2492e215d85ea2767e4bb8f9794c5ca08283
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-03-05 22:54:39 +1000

    Added num 23

---
+++ DZ23/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+System.Console.Write("Числа:");
+
+for (int i = 1; i < n+1; i++)
+{
+    System.Console.Write(Math.Pow(i,3)+" ");
+}
+
+System.Console.WriteLine();
