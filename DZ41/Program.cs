720d6a97ef0269a582019ec7a314376036163d24
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-03-15 18:37:53 +1000

    Added 41

---
+++ DZ41/Program.cs
@@ -0,0 +1,19 @@
+﻿
+System.Console.Write("Количество чисел = ");
+int m = int.Parse(Console.ReadLine());
+
+int [] a = new int [m];
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    System.Console.Write($"a[{i}] = ");
+    a[i]= int.Parse(Console.ReadLine());
+
+    if (a[i]>0)
+    {
+        count++;
+    }
+}
+
+System.Console.WriteLine($" -> {count}");
