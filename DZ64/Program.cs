38345f29e7667d1c3acb3f71bbc041595043a4a3
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-04-06 01:49:05 +1000

    DZ64

---
+++ DZ64/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+string NumbersRec(int b)
+{
+    if(1<=b)
+     return $"{b}, " + NumbersRec(b-1);
+    else return string.Empty;
+}
+
+System.Console.WriteLine(NumbersRec(n));
