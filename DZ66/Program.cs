81578617243eda25de59ff32fb2ed8b0235641bb
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-04-06 02:02:27 +1000

    DZ66

---
+++ DZ66/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+int SumRec(int m, int n)
+{
+    if (n==m-1) return 0;
+    else return n + SumRec(m,n-1);
+}
+System.Console.WriteLine(SumRec(m,n));
+
