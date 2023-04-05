e6ef0bab48e24d44ce24e4240e5d26d9e58c9f67
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-04-06 02:17:25 +1000

    DZ68

---
+++ DZ68/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+System.Console.Write($"A({m},{n}) = ");
+
+int Akkerman(int m, int n)
+{
+    if(m==0)
+        return n+1;
+    if(m>0&&n==0)
+        return Akkerman(m-1,1);
+    if(m>0&&n>0)
+        return Akkerman(m-1,Akkerman(m,n-1));
+    else return 0;
+}
+System.Console.WriteLine(Akkerman(m,n));
+
